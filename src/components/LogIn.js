51e4b0a4dd2b9acd6f0b3468433e2ebe78aedbe3
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-12-20 15:40:13 -0500

    add login screen

---
+++ src/components/LogIn.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+class LogIn extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            userName: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            userName: e.target.value
+        })
+    }
+
+    handleSubmit(e) {
+        if (e.keyCode === 13) {
+            this.props.logIn(this.state.userName);
+        }
+    }
+
+    render() {
+        return (
+            <div className="login">
+                <h1>Enter your username</h1>
+                <input
+                    value={this.state.userName}
+                    spellCheck="false"
+                    maxLength="15"
+                    onChange={this.handleChange}
+                    onKeyDown={this.handleSubmit}/>
+            </div>
+        )
+    }
+}
+
+export default LogIn;
