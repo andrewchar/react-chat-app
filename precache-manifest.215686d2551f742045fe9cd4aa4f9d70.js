a18b99a74ed6108f5d6e25190e412df394756075
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-12-20 15:49:16 -0500

    Updates

---
+++ precache-manifest.215686d2551f742045fe9cd4aa4f9d70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfc3670a8c9bbbab392164f4d485155b",
+    "url": "/react-chat-app/static/media/text-message.cfc3670a.svg"
+  },
+  {
+    "revision": "cce9c06f5a2e72a3b90e2598aeb43aa7",
+    "url": "/react-chat-app/static/media/send-button.cce9c06f.svg"
+  },
+  {
+    "revision": "e2a219927225f2f3d518",
+    "url": "/react-chat-app/static/js/runtime~main.e2a21992.js"
+  },
+  {
+    "revision": "6021189398bc84c29590",
+    "url": "/react-chat-app/static/js/main.60211893.chunk.js"
+  },
+  {
+    "revision": "79ce08a7f701e5107621",
+    "url": "/react-chat-app/static/js/1.79ce08a7.chunk.js"
+  },
+  {
+    "revision": "6021189398bc84c29590",
+    "url": "/react-chat-app/static/css/main.abf7be50.chunk.css"
+  },
+  {
+    "revision": "65d95dc7decd76ca83320ac66380bc30",
+    "url": "/react-chat-app/index.html"
+  }
+];
